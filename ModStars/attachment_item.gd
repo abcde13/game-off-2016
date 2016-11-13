06023b848b3bc6630c6fd987cbb8c0a46239b2a5
Suchaaver Chahal <schahal@elrel.com>
2016-11-13 09:24:04 -0800

    still working on colliders

--- ModStars/attachment_item.gd
+++ ModStars/attachment_item.gd
@@ -1,7 +1,14 @@
+# Used for all attachment types right now. 
+
 extends TextureButton
 
 signal attachment_change
 
+func _ready():
+	self.connect("attachment_change", get_parent().get_parent().get_node("editor_window"), "_on_attachment_change")
+
 func _pressed():
 	Input.set_custom_mouse_cursor(self.get_normal_texture())
 	emit_signal("attachment_change", self.get_name())
+	
+	
