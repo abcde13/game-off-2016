024a217e3d25bd36ec235b161443552977cdb95c
Suchaaver Chahal <suchaaverchahal@gmail.com>
2016-11-16 18:27:09 -0800

    trying to select sprite

--- ModStars/editor_window.tres.gd
+++ ModStars/editor_window.tres.gd
@@ -9,16 +9,8 @@ var gun_base
 var attachment = preload("res://test_area_scene.tscn")
 
 func _ready():
-	gun_base = get_node("gun_base")
-	set_process(true)
-	
-func _process(delta):
-	update()
-
-func _test_gun_overlap(new_piece):
-	print(gun_base.get_overlapping_areas())
-	return new_piece.overlaps_area(gun_base)
-	
+	gun_base = get_node("gun/gun_base")
+	gun_base.connect("input_event", gun_base, "select_attachmt")
 
 func _on_attachment_change(mode):
 	self.mode = mode
@@ -31,18 +23,24 @@ func _input_event(event):
 		if (mode):
 			var new_attachment = attachment.instance()
 			new_attachment.get_node("Sprite").set_texture(current_mode_tex)
-			var shape = RectangleShape2D.new()
-			shape.set_extents(current_mode_tex.get_size()/2)
-			new_attachment.add_shape(shape)
-			new_attachment.translate(event.pos)
+			new_attachment.get_shape(0).set_extents(current_mode_tex.get_size()/2)
+			get_node("gun").add_child(new_attachment)
+			new_attachment.set_pickable(true)
+			new_attachment.set_monitorable(true)
+			new_attachment.set_enable_monitoring(true)
+			new_attachment.connect("mouse_enter", gun_base, "select_attachment")
+			new_attachment.translate(event.pos - new_attachment.get_parent().get_pos())
 			
-
-		
-			add_child(new_attachment)
 			if(!new_attachment.get_shape(0).collide(new_attachment.get_transform(), gun_base.get_shape(0), gun_base.get_transform())):
 				new_attachment.free()
+			else:
+				mode = null
+				#Input.set_custom_mouse_cursor(load("res://icon.png"))
+				
 			
 				
+func select_attachment(viewport, event, shape_id):
+	print("selected")
 
 				
 func _on_mouse_enter():
