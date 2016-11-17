035aceff9f34e39700cc1ad5986dc53e40ba2db8
Suchaaver Chahal <schahal@elrel.com>
2016-11-16 23:27:31 -0800

    move and rotate working

--- ModStars/editor_window.tres.gd
+++ ModStars/editor_window.tres.gd
@@ -6,37 +6,88 @@ var attch
 var attch_collision
 var attch_area
 var gun_base
+var gun
+var edit_mode = null
 var attachment = preload("res://test_area_scene.tscn")
+var current_attachment
 
 func _ready():
+	gun = get_node("gun")
 	gun_base = get_node("gun/gun_base")
-	gun_base.connect("input_event", gun_base, "select_attachmt")
+	gun_base.connect("input_event", self, "select_attachment")
+	set_fixed_process(true)
 
 func _on_attachment_change(mode):
 	self.mode = mode
 	current_mode_tex = get_parent().get_node("attachments_window/" + mode).get_normal_texture()
 	print(mode)
 	
-func _input_event(event):
-	if (event.type == InputEvent.MOUSE_BUTTON and event.pressed):
-		accept_event()
-		if (mode):
-			var new_attachment = attachment.instance()
-			new_attachment.get_node("Sprite").set_texture(current_mode_tex)
-			new_attachment.get_shape(0).set_extents(current_mode_tex.get_size()/2)
-			get_node("gun").add_child(new_attachment)
-			new_attachment.set_pickable(true)
-			new_attachment.set_monitorable(true)
-			new_attachment.set_enable_monitoring(true)
-			new_attachment.connect("mouse_enter", gun_base, "select_attachment")
-			new_attachment.translate(event.pos - new_attachment.get_parent().get_pos())
-			
-			if(!new_attachment.get_shape(0).collide(new_attachment.get_transform(), gun_base.get_shape(0), gun_base.get_transform())):
-				new_attachment.free()
-			else:
-				mode = null
-				#Input.set_custom_mouse_cursor(load("res://icon.png"))
+	
+	
+func _fixed_process(delta):
+	if (current_attachment):
+		if (Input.is_action_pressed("SHIFT_UP")):
+			if(edit_mode == "MOVE"):
+				current_attachment.translate(Vector2(0,-1))
+			elif(edit_mode == "ROTATE"):
+				current_attachment.rotate(0.05)
+		elif (Input.is_action_pressed("SHIFT_DOWN")):
+			if(edit_mode == "MOVE"):
+				current_attachment.translate(Vector2(0,1))
+			elif(edit_mode == "ROTATE"):
+				current_attachment.rotate(-0.05)
+		elif (Input.is_action_pressed("SHIFT_LEFT")):
+			if(edit_mode == "MOVE"):
+				current_attachment.translate(Vector2(-1, 0))
+			elif(edit_mode == "ROTATE"):
+				current_attachment.rotate(0.05)
+		elif (Input.is_action_pressed("SHIFT_RIGHT")):
+			if(edit_mode == "MOVE"):
+				current_attachment.translate(Vector2(1, 0))
+			elif(edit_mode == "ROTATE"):
+				current_attachment.rotate(-0.05)
+		elif (Input.is_action_pressed("MOVE_MODE")):
+			edit_mode = "MOVE"
+		elif (Input.is_action_pressed("ROTATE_MODE")):
+			edit_mode = "ROTATE"
+		
+		var invalid = true	
+		for child in gun.get_children():
+			if (child != current_attachment):
+				if(child.get_shape(0).collide(child.get_transform(), current_attachment.get_shape(0), current_attachment.get_transform())):
+					invalid = false
+					break
 				
+		if (invalid):
+			current_attachment.get_node("Sprite").set_modulate(Color(0.3,0,0,1))
+		else:
+			current_attachment.get_node("Sprite").set_modulate(Color(1,1,1,1))
+	
+		
+func _input_event(event):
+	print(event)
+	if (event.type == InputEvent.MOUSE_BUTTON and event.pressed and mode):
+		accept_event()
+		var new_attachment = attachment.instance()
+		new_attachment.get_node("Sprite").set_texture(current_mode_tex)
+		new_attachment.get_shape(0).set_extents(current_mode_tex.get_size()/2)
+		get_node("gun").add_child(new_attachment)
+		new_attachment.set_pickable(true)
+		new_attachment.set_monitorable(true)
+		new_attachment.set_enable_monitoring(true)
+		new_attachment.connect("input_event", self, "select_attachment")
+		new_attachment.translate(event.pos - new_attachment.get_parent().get_pos())
+		
+		if(!new_attachment.get_shape(0).collide(new_attachment.get_transform(), gun_base.get_shape(0), gun_base.get_transform())):
+			new_attachment.free()
+		else:
+			mode = null
+			edit_mode = "MOVE"
+			current_attachment = new_attachment
+			#Input.set_custom_mouse_cursor(load("res://icon.png"))
+	
+			
+	
 			
 				
 func select_attachment(viewport, event, shape_id):
--- ModStars/test_area.gd
+++ ModStars/test_area.gd
@@ -5,6 +5,3 @@ func _ready():
 	
 func eee(who):
 	print(who.overlaps_area(self))
-	print(who.get_name() + "goo goo ")
-	if (who.get_name() == "gun_base"):
-		get_node("Sprite").set_modulate(Color(0.8, 0, 0, .5))
